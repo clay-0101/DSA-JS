7e1089868a1518aa5874bf014cd62d6e4b7bd921
clay-0101 <karyon.crypten.dev0111@gmail.com>
2026-04-19 21:42:19 +0530

    Add String Questions

---
+++ String/string.js
@@ -0,0 +1,33 @@
+// Ouestion 1.) Print every character of string on a new Line
+// let prompt = require('prompt-sync')()
+// let str = prompt('Enter Your String : ') 
+
+// for(let i = 0 ; i < str.length; i++){
+//     console.log(str.charAt(i))
+// }
+
+//Ousetion 2.) Print string in the reverse order 
+// let prompt = require('prompt-sync')()
+// let str = prompt('Enter Your String : ') 
+// let revStr = ''
+
+// for(let i = str.length -1; i >= 0; i--){
+//     revStr += str.charAt(i)
+// }
+// console.log(revStr)
+
+//Ousetion 2.) Check Palindromic Sring or not 
+// let prompt = require('prompt-sync')()
+// let str = prompt('Enter Your String : ').toLocaleLowerCase() 
+// let revStr = ''
+
+// for(let i = str.length -1; i >= 0; i--){
+//     revStr += str.charAt(i)
+// }
+// if(str == revStr) console.log('Palindromic String')
+// else{
+//     console.log("Not Palindromic String")
+// }
+
+
+
